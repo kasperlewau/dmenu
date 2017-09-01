d816940918273976fa3487df6bc55b824e5974ea
Kasper Lewau <lewauu@gmail.com>
2017-09-01 10:00:33 +0200

    init

--- config.h
+++ config.h
@@ -4,17 +4,21 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Droid Sans Mono for Powerline:size=8"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char col_white[] = "#ffffff";
+static const char col_black[] = "#1d1f21";
+static const char col_gray[] = "#373b41";
+
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { col_white, col_gray },
+	[SchemeSel] = { col_white, col_black },
+	[SchemeOut] = { col_white, col_black },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 10;
 
 /*
  * Characters not considered part of a word while deleting words
